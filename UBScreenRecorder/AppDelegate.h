5bddb390ed02fff5b781dcd32a903428e464e050
Sam Gray <samleegray@icloud.com>
2015-03-10 14:15:24 -0700

    Initial Commit

---
+++ UBScreenRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UBScreenRecorder
+//
+//  Created by Sam Gray on 3/10/15.
+//  Copyright (c) 2015 UberBit LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
